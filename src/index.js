eef29ad9c2a6b41f741937eceb4a3722b6e67f08
natu.shubham96 <shubhamnatu52@gmail.com>
2019-08-06 19:39:40 +0530

    Logging Errors with Sentry.io and Reven

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Raven from 'raven-js';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+Raven.config ('https://9aa614c184fb4b9f9ecd0a06afcd2033@sentry.io/1523025', {
+  release: '1-0-0', //this release indicates the version of our application
+  environment: 'development-test',
+}).install ();
+
+ReactDOM.render (<App />, document.getElementById ('root'));
+registerServiceWorker ();
